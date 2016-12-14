57f85ec8337c5fb1fff3875480c3dbc892a73d68
jemcws <v-sutat@microsoft.com>
2016-12-14 18:55:04 +0530

    Update Create_RG_SA.ps1

--- CustomCWsScripts/Create_RG_SA.ps1
+++ CustomCWsScripts/Create_RG_SA.ps1
@@ -8,50 +8,16 @@ param(
  [String] $SPPwd=""
 )
 try{
-    <#
-    .SYNOPSIS
-        Registers RPs
-    #>
-    Function RegisterRP {
-        Param(
-            [string]$ResourceProviderNamespace
-        )
-
-        Write-Host "Registering resource provider '$ResourceProviderNamespace'";
-        Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
-    }
-       $ErrorActionPreference = "Stop"
    
-    # sign in
-    Write-Host "Logging in...";
-    # Login-AzureRmAccount;
-    try {
-    #Check if the user is already logged in for this session
-    $AzureRmContext = Get-AzureRmContext | out-null
-    Write-verbose "Connected to Azure"
-    } 
-    catch 
-    {    
-	    $user= $SPName
-	    $pass = ConvertTo-SecureString $SPPwd -AsPlainText –Force
-	    $cred = New-Object -TypeName pscredential –ArgumentList $user, $pass
-	    Login-AzureRmAccount -Credential $cred -ServicePrincipal –TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47
-	     Write-verbose "logged into Azure."
-	    $error.Clear()
-    }
+    $user= $SPName
+    $pass = ConvertTo-SecureString $SPPwd -AsPlainText –Force
+    $cred = New-Object -TypeName pscredential –ArgumentList $user, $pass
+    Login-AzureRmAccount -Credential $cred -ServicePrincipal –TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47
+    Write-verbose "logged into Azure."
     
     # select subscription
     Write-Host "Selecting subscription '$subscriptionId'";
     Select-AzureRmSubscription -SubscriptionID $subscriptionId;
-
-    # Register RPs
-    $resourceProviders = @("microsoft.compute","microsoft.network","microsoft.storage");
-    if($resourceProviders.length) {
-        Write-Host "Registering resource providers"
-        foreach($resourceProvider in $resourceProviders) {
-            RegisterRP($resourceProvider);
-        }
-    }
 
     #Create or check for existing resource group
     $resourceGroup = Get-AzureRmResourceGroup -Name "$resourceGroupName" -ErrorAction SilentlyContinue
@@ -79,7 +45,7 @@ try{
      }
      else
      {
-      Write-Host "Taking already existing Storage Account: '$StorageAccountName' from  Resource Group: '$resourceGroupName' "
+        Write-Host "Taking already existing Storage Account: '$StorageAccountName' from  Resource Group: '$resourceGroupName' "
      }
 }
 catch [System.Exception]
